0c5611d1793f98f5e484d0c8bb1fb2c9b4b973c2
zhong.xie <zhong.xie@qq.com>
2020-11-12 17:24:05 +0800

    新增mvn_deploy.bat

---
+++ mvn_deploy.bat
@@ -0,0 +1,83 @@
+@echo deploy gvdroid-base
+cd ./gvdroid-base
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-dependencies
+cd ../gvdroid-boot-dependencies
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-parent
+cd ../gvdroid-boot-parent
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter-parent
+cd ../gvdroid-boot-starter-parent
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starters
+cd ../gvdroid-boot-starters
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter-util
+cd ../gvdroid-boot-starter-util
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter-validation
+cd ../gvdroid-boot-starter-validation
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter-redis
+cd ../gvdroid-boot-starter-redis
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter-logging
+cd ../gvdroid-boot-starter-logging
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter-mybatis
+cd ../gvdroid-boot-starter-mybatis
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter-security
+cd ../gvdroid-boot-starter-security
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter-helper
+cd ../gvdroid-boot-starter-helper
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter-autoconfigure
+cd ../gvdroid-boot-starter-autoconfigure
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter-desire
+cd ../gvdroid-boot-starter-desire
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-boot-starter
+cd ../gvdroid-boot-starter
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-cloud-dependencies
+cd ../gvdroid-cloud-dependencies
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-cloud-parent
+cd ../gvdroid-cloud-parent
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-cloud-starters
+cd ../gvdroid-cloud-starters
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-cloud-starter-autoconfigure
+cd ../gvdroid-cloud-starter-autoconfigure
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+@echo deploy gvdroid-cloud-starter
+cd ../gvdroid-cloud-starter
+call mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests
+
+
+@echo deploy completed.
+cd ..
