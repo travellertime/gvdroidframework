b966d8e2f0a20503bf5442e2d61f323f6e0bd03a
zhong.xie <zhong.xie@qq.com>
2020-11-13 19:03:45 +0800

    增加注释

--- gvdroid-boot-starter-desire/src/main/java/com/gvdroidframework/desire/core/SerialTemplate.java
+++ gvdroid-boot-starter-desire/src/main/java/com/gvdroidframework/desire/core/SerialTemplate.java
@@ -1,8 +1,26 @@
 package com.gvdroidframework.desire.core;
 
+/**
+ * 序号服务
+ */
 public interface SerialTemplate {
 
+    /**
+     * 根据传入的主键获取序号，每查询一次序号自动加一
+     *
+     * @param key       主键
+     * @param valueDate 日期 format: 20201111
+     * @return 序号
+     */
     Integer serialInquiry(String key, String valueDate);
 
+    /**
+     * 根据传入的主键获取序号，当addRequired为true时，每查询一次序号自动加一
+     *
+     * @param key         主键
+     * @param valueDate   日期 format: 20201111
+     * @param addRequired 序号是否需要加一
+     * @return 序号
+     */
     Integer serialInquiry(String key, String valueDate, boolean addRequired);
 }
