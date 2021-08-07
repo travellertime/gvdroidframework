665e989dd1eefa6c1c653283973b0d65b17ef3a7
xiezhong <zhong.xie@qq.com>
2021-08-07 11:09:42 +0800

    add license

--- gvdroid-base/src/main/java/com/gvdroidframework/base/component/R.java
+++ gvdroid-base/src/main/java/com/gvdroidframework/base/component/R.java
@@ -1,7 +1,30 @@
+/*
+ * Copyright 2014-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.gvdroidframework.base.component;
 
 import java.io.Serializable;
 
+/**
+ * 返回对象封装类
+ *
+ * @author Colin
+ * @since 2.0.0
+ *
+ * @param <T> 返回对象
+ */
 public final class R<T> implements Serializable {
     private Status status;
     private T body;
--- gvdroid-boot-starter-autoconfigure/src/main/java/com/gvdroidframework/boot/autoconfigure/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.gvdroidframework.boot.autoconfigure;
